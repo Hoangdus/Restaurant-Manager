abd5c1f34f7c4589940d9f471680a5958e434e03
truongdka1308 <truongngv1308@gmail.com>
2024-09-27 21:50:58 +0700

    add model bill,billDetail,ingredient
    add model

--- models/ingredient.js
+++ models/ingredient.js
@@ -1,1 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
+const ingredientSchema = new Schema({
+  ingredient_name: {
+    type: String,
+    required: true 
+  },
+  ingredients_amount: {
+    type: Number,
+    required: true 
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('ingredient', ingredientSchema);
