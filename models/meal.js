d71a623a1e15ad36af6adf73b0594b10928e415f
Hoangdus <71692001+Hoangdus@users.noreply.github.com>
2024-09-27 21:45:41 +0700

    Update meal.js

--- models/meal.js
+++ models/meal.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const meal = new Schema({
-    name:{type: String},
-    price: String,
+    name: String,
+    price: Number,
     status: Number,
     type_id: {type: Schema.Types.ObjectId, ref:'meal_type'},
     info: String,
